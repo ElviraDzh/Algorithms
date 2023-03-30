1ff615a8f2339d00ebf789f174e01f97d6ccfb5d
ElviraDzh <elvira.jumalieva@gmail.com>
2023-03-30 21:08:07 +0100

    Reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.

--- reverse-order-characters-in-word.js
+++ reverse-order-characters-in-word.js
@@ -15,11 +15,27 @@
 // There is at least one word in s.
 // All the words in s are separated by a single space.
 
-function reverseOrderOfCharacters(s) {
+//1-way with chartAt()
+function reverseWords(s) {
   let newString = "";
 
   for (let i = s.length - 1; i >= 0; i--) {
     newString += s.charAt(i);
   }
   return newString.split(" ").reverse().join(" ");
+}
+//2-way
+function reverseWords(s) {
+  const arr = s.split(" ");
+
+  const reverseArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let reverseWord = "";
+    for (let j = arr[i].length - 1; j >= 0; j--) {
+      reverseWord += arr[i][j];
+    }
+    reverseArr.push(reverseWord);
+  }
+  return reverseArr.join(" ");
 }
