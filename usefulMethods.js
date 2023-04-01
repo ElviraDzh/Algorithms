783d31de37b662e5c07d738d4a7504364b1dc41c
ElviraDzh <elvira.jumalieva@gmail.com>
2023-04-01 23:12:29 +0100

    Find average num in an array.

--- usefulMethods.js
+++ usefulMethods.js
@@ -38,3 +38,12 @@ function min(array) {
   }
   return minNum;
 }
+
+function average(array) {
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total / array.length;
+}
