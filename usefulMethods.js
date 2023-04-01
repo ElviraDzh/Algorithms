bd10c1feb81b0e03488cdaab311f0984f938ff7c
ElviraDzh <elvira.jumalieva@gmail.com>
2023-04-01 23:13:28 +0100

    Find total num in an array.

--- usefulMethods.js
+++ usefulMethods.js
@@ -47,3 +47,12 @@ function average(array) {
   }
   return total / array.length;
 }
+
+function total(array) {
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+}
